fb1df1288db86b9bc276f5a3000d4835fcb07602
santosh.kumar09@sap.com-at-446084804320 <santosh.kumar09@sap.com>
2020-06-11 17:29:50 +0530

    Initial commit

---
+++ src/main/java/com/sap/springbrewerygateway/SpringBreweryGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.sap.springbrewerygateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBreweryGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBreweryGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sap/springbrewerygateway/SpringBreweryGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sap.springbrewerygateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBreweryGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
